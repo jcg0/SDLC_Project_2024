cdaf146643c094a3f05cbd3214a346f454dbed30
jcg0 <jg.awsprod@gmail.com>
2024-10-27 09:15:37 -0400

    fixed the server and now it and the DB are working

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = process.env.JAWSDB_URL 
-    ? new Sequelize(process.env.JAWSDB_URL) 
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: '127.0.0.1',
-        dialect: 'mysql',
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "127.0.0.1",
+        dialect: "mysql",
+        // port: "3006",
         dialectOptions: {
-            decimalNumbers: true,
-            socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+          decimalNumbers: true,
+          // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
         },
-    });
+      }
+    );
 
 module.exports = sequelize;
